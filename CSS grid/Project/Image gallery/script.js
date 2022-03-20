47321bdccecf08621464889bf556d6346017988d
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-03-20 09:00:19 +0530

    added modal and project completed

--- CSS grid/Project/Image gallery/script.js
+++ CSS grid/Project/Image gallery/script.js
@@ -1,93 +1,57 @@
-// for(let i=1;i<=5;i++){
+let container = document.querySelector(".container")
 
-// let card = document.createElement("div")
+const dir = "images/"
 
-// card.setAttribute("class",`card card-${i}`)
-// card.setAttribute("id","neu")
+// creating cards
 
-// let img = document.createElement("img")
-// img.src = img_src+i+".jpg"
-
-// card.append(img)
-
-// container.appendChild(card)
-// }
-
-
-function first_way() {
-    let container = document.querySelector(".container")
-
-
-    const dir = "images/"
-
-    // creating cards
-
-    // const data = fetch("details.json");
-    const img_src = dir + "img-";
-    fetch("/details.json").then((res) => res.json()).then((res) => {
-
-        res.portfolio.map(({ name, role }, index) => {
+// const data = fetch("details.json");
+const img_src = dir+"img-";
+for(i=0;i<=4;i++){
+    fetch("/details.json").then((res)=>res.json()).then((res)=>{
+    
+        res.portfolio.map(({name,role},index)=>{
             let card = document.createElement("div")
-
-            card.setAttribute("class", `card card-${index + 1}`)
-            card.setAttribute("id", "neu")
-
+        
+            card.setAttribute("class",`card card-${index+1}`)
+            card.setAttribute("id","neu")
+            card.addEventListener("click",(e)=>{
+                click(e)
+            })
+        
             let img = document.createElement("img")
-
-            i = index + 1
-            img.src = img_src + i + ".jpg"
-
-            let p1 = `<p>Name - ${name}</p>`
-            let p2 = `<p>Role - ${role}</p>`
+            img.setAttribute("id","img")
+            i=index+1
+            img.src = img_src+i+".jpg"
+            
+            let p1 = `<p id="name">Name - ${name}</p>`
+            let p2 = `<p id="role">Role - ${role}</p>`
             card.append(img)
-            card.innerHTML += p1
-            card.innerHTML += p2
-
-
+            card.innerHTML+=p1
+            card.innerHTML+=p2
+            
+    
             container.appendChild(card)
         })
     })
 
 }
 
-function second_way() {
-    let container = document.querySelector(".container")
+function click(e){
+    const dialog = document.querySelector("dialog")
 
-
-    const dir = "images/"
-
-    // creating cards
-
-    // const data = fetch("details.json");
-    const img_src = dir + "img-";
-
-    const req_data = fetch("/details.json").then((res) => res.json())
-
-    Promise.resolve(req_data).then((res) => {
-        res.portfolio.map(({ name, role }, index) => {
-            let card = document.createElement("div")
-
-            card.setAttribute("class", `card card-${index + 1}`)
-            card.setAttribute("id", "neu")
-
-            let img = document.createElement("img")
-
-            i = index + 1
-            img.src = img_src + i + ".jpg"
-
-            let p1 = `<p>Name - ${name}</p>`
-            let p2 = `<p>Role - ${role}</p>`
-            card.append(img)
-            card.innerHTML += p1
-            card.innerHTML += p2
-
-
-            container.appendChild(card)
-        })
+    const close = document.createElement("button")
+    close.innerText="close"
+    close.setAttribute("id","close")
+    close.addEventListener("click",()=>{
+        dialog.innerHTML=""
+        dialog.removeAttribute("id","dialog")
+        dialog.close()
     })
+    dialog.setAttribute("id","dialog")
+    
+    const parent = e.target.innerHTML
+    dialog.innerHTML = parent
+    dialog.append(close)
+    dialog.showModal()
 }
-
-first_way()
-second_way()
-
 
