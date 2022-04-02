3cb2540c973163eb538947becfbc22e348382057
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-04-02 14:03:45 +0530

    MERN avoided

--- JS/Communication tech/Fetch api/Loading spinners using fetch/script.js
+++
@@ -1,9 +0,0 @@
-let btn = document.getElementById("btn")
-let place = document.getElementById("place")
-
-function get_data(){
-    place.innerText = "Loading...."
-    const url = " http://127.0.0.1:5000/"
-    fetch(url).then(res => res.text()).then(data => {place.innerText =data })
-}
-btn.onclick = get_data
--- JS/Communication tech/websockets/project + lessons/client(basics)/script.js
+++
@@ -1,7 +0,0 @@
-// Create WebSocket connection.
-const ws = new WebSocket("ws://localhost:9876")
-
-// after the ws is ready
-ws.onopen = ()=>{
-    ws.onmessage = response => console.log(response.data)
-}
